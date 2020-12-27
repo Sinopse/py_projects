da1a943ce62fc31518b037ec42933680f9b70bfb
Sinopse <aleksander.tarasov9l@gmail.com>
2020-12-27 15:02:30 +0100

    usage of defaultdict

---
+++ defaultdict_setdefault.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+class Visits:
+    def __init__(self):
+        self.data = defaultdict(set)
+
+    def add(self, country, city):
+        self.data[country].add(city)
+
+
+visits = Visits()
+visits.add('Russia', 'Moscow')
+visits.add('Russia', 'Bryansk')
+visits.add('Germany', 'Berlin')
+print(visits.data['Russia'])
