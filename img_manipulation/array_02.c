734b63f0c1c2fa2f4bb14696864cdf5387a7ea1e
Sinopse <aleksander.tarasov9l@gmail.com>
2021-05-20 09:50:12 +0200

    array_02.c new version

--- img_manipulation/array_02.c
+++ img_manipulation/array_02.c
@@ -8,7 +8,32 @@ void fillCol(unsigned *** arr, size_t col, size_t row) {
     arr[i] = rowarr;
     rowarr = NULL;
   }
+}
 
+void fillRow(unsigned *** arr, size_t col, size_t row) {
+  size_t pxl = 3;
+  unsigned * pxlarr = NULL;
+  for (int k = 0; k < col; k++) {
+    for (int i = 0; i < row; i++) {
+      pxlarr = realloc(pxlarr, pxl * sizeof(*pxlarr));
+      // pass in 0 values to pxlarr
+      for (int j = 0; j < pxl; j++) {
+	pxlarr[j] = 0;
+      }
+      arr[k][i] = pxlarr;
+      pxlarr = NULL;
+    }
+  }
+}
+
+void freeArr(unsigned *** arr, size_t col, size_t row) {
+   for (int i = 0; i < col; i++) {
+     for (int j = 0; j < row; j++) {
+       free(arr[col][row]);
+       }
+     free(arr[col]);
+     }
+   free(arr);
 }
 
 int main() {
@@ -18,12 +43,8 @@ int main() {
   unsigned *** colarr = malloc(col * sizeof(*colarr));
   
   fillCol(colarr, col, row);
-
-  for (int i = 0; i < col; i++) {
-    //      for (int j = 0; j < col; j++) {
-    free(colarr[i]);
-  }
-  free(colarr);
+  fillRow(colarr, col, row);
+  freeArr(colarr, col, row);
 
   return 0;
 }
